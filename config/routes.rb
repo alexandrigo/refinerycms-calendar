05fcc6411bdef7e9f8c266e2096dfbfb556b3c30
Alexander Alexeev <alexander.alexeev@railsware.com>
2013-09-24 01:13:31 +0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Refinery::Core::Engine.routes.append do
+
+  # Frontend routes
+  namespace :calendar do
+    get 'events/archive' => 'events#archive'
+    resources :events, :only => [:index, :show]
+  end
+
+  # Admin routes
+  namespace :calendar, :path => '' do
+    namespace :admin, :path => 'refinery/calendar' do
+      resources :events, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
+    end
+  end
+
+  # Admin routes
+  namespace :calendar, :path => '' do
+    namespace :admin, :path => 'refinery/calendar' do
+      resources :venues, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
+    end
+  end
+
+end
